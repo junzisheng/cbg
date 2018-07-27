7bd2946c71629e3dd6102d80c55a5ebebaeb1416
namibox <test@qq.com>
2018-07-27 23:58:17 +0800

    pass

--- crawl_celery/__init__.py
+++ crawl_celery/__init__.py
@@ -1,5 +1,5 @@
 import os, sys
-if 'worker' in sys.argv:  # celery开启需要初始化django模型
+if 'worker' in sys.argv or 'flower' in sys.argv:  # celery开启需要初始化django模型
 # if True:
     from cbg_backup import settings
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cbg_backup.settings")
